b9b49f92c67f28ecd3ed50ace484e79a2e3c7cc2
Sergey Bakhar <b113.bahar@gmail.com>
2019-03-09 21:08:10 +0200

    finish social share buttons

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -4,6 +4,17 @@ import ImageGallery from 'react-image-gallery';
 import '../../../node_modules/react-image-gallery/styles/scss/image-gallery.scss';
 import GoogleMapReact from 'google-map-react';
 import { FacebookProvider, Comments } from 'react-facebook';
+import {
+  FacebookShareButton,
+  TelegramShareButton,
+  WhatsappShareButton,
+  ViberShareButton,
+  FacebookIcon,
+  TelegramIcon,
+  WhatsappIcon,
+  ViberIcon
+} from 'react-share';
+
 
 class Card extends Component {
 
@@ -53,6 +64,8 @@ class Card extends Component {
         <h1 className={styles.card__header}>Заголовок poskot travel</h1>
         {/* <span className={styles.card__address}>Адрес:</span> */}
         <p className={styles.card__description}>Описание: Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quis, non aperiam ipsa illo excepturi aliquam mollitia asperiores eos qui placeat aut adipisci blanditiis maxime sit voluptas provident enim laborum officiis aliquid vitae temporibus! Recusandae!</p>
+
+        <h2 className={styles.card__title}>Галерея</h2>
         <ImageGallery
           items={images}
           showThumbnails={false}
@@ -61,23 +74,61 @@ class Card extends Component {
           showBullets
           additionalClass={styles.card__slider}
         />
+
+        <h2 className={styles.card__title}>Где искать</h2>
         <div className={styles.card__map}>
-          <div className={styles['card__map-wrapper']}>
-            <GoogleMapReact
+          <div className={styles['card__map-inner']}>
+            {/* <GoogleMapReact
               bootstrapURLKeys={{ key: 'AIzaSyAPSumGDQfNDZT9TlkfWoROc5V1who_t9M' }}
               defaultCenter={this.props.center}
               defaultZoom={this.props.zoom}
               yesIWantToUseGoogleMapApiInternals
               onGoogleApiLoaded={({ map, maps }) => this.renderMarkers(map, maps)}
-            />
+            /> */}
           </div>
         </div>
-        <div className={styles.card__fb}>
+
+        <h2 className={styles.card__title}>Поделиться</h2>
+        <div className={styles['card__social-share']}>
+          <FacebookShareButton
+            url='http://github.com'
+            quote='test'
+            className={styles['card__social-share-item']}
+          >
+            <FacebookIcon size={40} />
+          </FacebookShareButton>
+
+          <TelegramShareButton
+            url='http://github.com'
+            title='test'
+            className={styles['card__social-share-item']}
+          >
+            <TelegramIcon size={40} />
+          </TelegramShareButton>
+
+          {/* <ViberShareButton
+              url='https://moz.com/blog/domain-authority-seo'
+             
+              className={styles['card__social-share-item']}
+            >
+              <ViberIcon size={40} />
+            </ViberShareButton> */}
+
+          <WhatsappShareButton
+            url='http://github.com'
+            title='test'
+            className={styles['card__social-share-item']}
+          >
+            <WhatsappIcon size={40} />
+          </WhatsappShareButton>
+        </div>
+
+        <h2 className={styles.card__title}>Оставить комментарий</h2>
+        <div className={styles['card__fb-comments']}>
           <FacebookProvider appId="1045257252264946" language='ru_RU'  >
             <Comments href="http://localhost:3000/" width='50%' numPosts='3' orderBy='social' />
           </FacebookProvider>
         </div>
-
       </div>
     );
   }
