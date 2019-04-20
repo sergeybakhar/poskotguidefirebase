ef4c94bba58e16ef71412afb6e2a7a208fc19926
Sergey Bakhar <b113.bahar@gmail.com>
2019-04-20 11:51:32 +0300

    fix cardlist and loader

--- src/components/Loader/index.js
+++ src/components/Loader/index.js
@@ -5,7 +5,7 @@ import styles from './Loader.module.scss'
 const Loader = () => {
     return (
         <div className={styles.loader}>
-            <img src={loader} alt="loader" />
+            <img src={loader} alt="loader" className={styles.loader__img} />
         </div>
     )
 }
