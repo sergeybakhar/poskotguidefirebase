fc65d6441fd17853ffc235a283f6e73a5fffabb9
Sergey Bakhar <b113.bahar@gmail.com>
2019-05-10 17:39:59 +0300

    start working on Intro section

---
+++ src/components/Intro/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Intro.module.scss'
+
+const Intro = () => {
+    return (
+        <div className={styles.intro}>
+        </div>
+    )
+}
+
+export default Intro
